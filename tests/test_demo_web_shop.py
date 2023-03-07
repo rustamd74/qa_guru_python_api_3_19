1e94e624e7b49c3b8868621533f551805e15b21f
Rustam Dzhafarov <rustamd74@gmail.com>
2023-03-07 11:36:58 +0300

    Homework 19

--- tests/test_demo_web_shop.py
+++ tests/test_demo_web_shop.py
@@ -1,4 +1,5 @@
 import os
+import allure
 
 from selene import have
 
@@ -7,26 +8,35 @@ PASSWORD = os.getenv('PASSWORD')
 
 
 def test_auth_login(auth_user):
-    auth_user.open('')
-    auth_user.element('.account').should(have.text(LOGIN))
+    with allure.step('Checking for Successful Authorization'):
+        auth_user.open('')
+        auth_user.element('.account').should(have.text(LOGIN))
 
 
 def test_add_to_cart(auth_user, demoshop):
-    demoshop.get('/notebooks')
-    auth_user.element('.button-2 ').click()
+    with allure.step('Product selection'):
+        demoshop.get('/notebooks')
+    with allure.step('Add to cart'):
+        auth_user.element('.button-2 ').click()
 
 
 def test_checking_shopping_cart(demoshop):
-    response = demoshop.post("/addproducttocart/catalog/31/1/1")
-    assert response.status_code == 200
+    with allure.step('Checking shopping cart'):
+        response = demoshop.post("/addproducttocart/catalog/31/1/1")
+        assert response.status_code == 200
 
 
 def test_update_shopping_cart(auth_user, demoshop):
-    demoshop.post('/addproducttocart/catalog/31/1/1')
-    auth_user.element('.ico-cart').click()
-    auth_user.element('.qty-input').clear().send_keys(0).press_enter()
-    auth_user.element('.order-summary-content').should(have.text('Your Shopping Cart is empty!'))
+    with allure.step('Finding an item in the shopping cart'):
+        demoshop.post('/addproducttocart/catalog/31/1/1')
+    with allure.step('Selecting product in cart'):
+        auth_user.element('.ico-cart').click()
+    with allure.step('Removing an item from the cart'):
+        auth_user.element('.qty-input').clear().send_keys(0).press_enter()
+    with allure.step('Checking if the cart is empty'):
+        auth_user.element('.order-summary-content').should(have.text('Your Shopping Cart is empty!'))
 
 
 def test_logout(demoshop):
-    demoshop.get('logout')
+    with allure.step('Checking the logout'):
+        demoshop.get('logout')
--- utils/base_session.py
+++ utils/base_session.py
@@ -1,5 +1,44 @@
+import json
+import logging
+from json import JSONDecodeError
+
 import allure
 from requests import Session, Response
+from allure_commons.types import AttachmentType
+from curlify import to_curl
+
+
+def allure_logger(function):
+    def wrapper(*args, **kwargs):
+        method, url = args[1], args[2]
+
+        with allure.step(f"{method} {url}"):
+
+            response: Response = function(*args, **kwargs)
+
+            allure.attach(body=to_curl(response.request).encode("utf8"), name=f"Request {response.status_code}",
+                          attachment_type=AttachmentType.TEXT, extension=".txt")
+            try:
+                allure.attach(body=json.dumps(response.json(), indent=4).encode("utf8"),
+                              name=f"Response {response.status_code}", attachment_type=AttachmentType.JSON,
+                              extension=".json")
+            except JSONDecodeError:
+                allure.attach(body=response.text.encode("utf8"), name=f"Response {response.status_code}",
+                              attachment_type=AttachmentType.TEXT, extension=".txt")
+
+        return response
+
+    return wrapper
+
+
+def request_logger(func):
+    def wrapper(*args, **kwargs):
+        response: Response = func(*args, **kwargs)
+        logging.info(f"status code is {response.status_code} - {to_curl(response.request)}")
+
+        return response
+
+    return wrapper
 
 
 class BaseSession(Session):
@@ -7,6 +46,10 @@ class BaseSession(Session):
         super(BaseSession, self).__init__()
         self.url = url
 
+    @allure_logger
+    @request_logger
     def request(self, method, url, **kwargs) -> Response:
-        with allure.step(f"{method} {url}"):
-            return super().request(method, self.url + url, **kwargs)
+        response = super().request(method, self.url + url, **kwargs)
+        logging.info(f'{response.status_code} {to_curl(response.request)}')
+
+        return response
